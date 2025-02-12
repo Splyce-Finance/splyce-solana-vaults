d0cf84f9c8fb1458a188bc023ce25349e01274c0
codinghistorian <gaudihaus@gmail.com>
2025-02-13 00:50:59 +0400

    report_profit fix
    -so that strategy.manager() will govern

--- programs/strategy/src/instructions/report_profit.rs
+++ programs/strategy/src/instructions/report_profit.rs
@@ -1,12 +1,7 @@
 use anchor_lang::prelude::*;
 use anchor_spl::token_interface::{Mint, TokenAccount, TokenInterface};
-use access_control::{
-    constants::USER_ROLE_SEED,
-    program::AccessControl,
-    state::{UserRole, Role}
-};
-
 use crate::utils::unchecked_strategy::UncheckedStrategy;
+use crate::error::ErrorCode;
 use crate::constants::UNDERLYING_SEED;
 
 #[derive(Accounts)]
@@ -21,25 +16,16 @@ pub struct ReportProfit<'info> {
     #[account(mut, constraint = underlying_mint.key() == strategy.underlying_mint())]
     pub underlying_mint: InterfaceAccount<'info, Mint>,
     
-    #[account(
-        seeds = [
-            USER_ROLE_SEED.as_bytes(), 
-            signer.key().as_ref(),
-            Role::StrategiesManager.to_seed().as_ref()
-        ], 
-        bump,
-        seeds::program = access_control.key()
-    )]
-    pub roles: Account<'info, UserRole>,
-
-    #[account(mut, constraint = roles.check_role()?)]
+    #[account(mut, constraint = signer.key() == strategy.manager() @ErrorCode::AccessDenied)]
     pub signer: Signer<'info>,
 
     pub token_program: Interface<'info, TokenInterface>,
-    pub access_control: Program<'info, AccessControl>,
 }
 
-pub fn handle_report_profit<'info>(ctx: Context<'_, '_, '_, 'info, ReportProfit<'info>>, profit: u64) -> Result<()> {
+pub fn handle_report_profit<'info>(
+    ctx: Context<'_, '_, '_, 'info, ReportProfit<'info>>, 
+    profit: u64
+) -> Result<()> {
     let mut strategy = ctx.accounts.strategy.from_unchecked()?;
 
     strategy.report_profit(&ctx.accounts, &ctx.remaining_accounts, profit)?;
